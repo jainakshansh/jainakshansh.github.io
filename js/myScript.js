f56677081bbb47878cf06570bfff586bd37658ce
Akshansh Jain <akshanshjn9@gmail.com>
2018-02-18 01:20:12 +0530

    Akshansh Jain Website

---
+++ js/myScript.js
@@ -0,0 +1,9 @@
+function open_nav() {
+	document.getElementById('navMenu').style.width="100px";
+	document.getElementById('body_container').style.marginTop="100px";
+}
+
+function close_nav() {
+	document.getElementById('navMenu').style.width="0px";
+	document.getElementById('body_container').style.marginTop="0px";
+}
